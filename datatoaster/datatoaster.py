48e6ef4f583bfd07710dc1996abc592e8a0dd64f
Null <abc612008@126.com>
2017-10-27 14:06:31 +0800

    more test cases & y function support

--- datatoaster/datatoaster.py
+++ datatoaster/datatoaster.py
@@ -3,6 +3,7 @@ class DataSet:
     NumberOfAppearance = 0
     Percentage = 1
     PercentageWithinGroup = 2
+    Single = lambda _: ""
 
     def __init__(self, raw_data):
         self.raw_data = raw_data
@@ -13,6 +14,7 @@ class DataSet:
         self.percentage_within_group = False
         self.constraints = []
         self.pre_constraints = []
+        self.single = False
 
     def set_x(self, func):
         if not callable(func):
@@ -65,13 +67,21 @@ class DataSet:
 
         return self
 
+    def set_single(self, param):
+        self.single = param
+        return self
+
     def get_result(self):
-        filtered_data = []
-        number_of_valid_data = 0
-        all_appearance = {}
+        if self.x_function is None:  # x_function should not be None
+            raise ValueError("set_x not called when calling get_result")
+
+        filtered_data = []  # data that passed all constraints
+        number_of_valid_data = 0  # save the total unfiltered number for percentage
+        all_appearance = {}  # save the unfiltered number per group for percentage_within_group
+
         for item in self.raw_data:
             pass_constraints = True
-            for pre_constraint in self.pre_constraints:
+            for pre_constraint in self.pre_constraints:  # pre constraints
                 if not pre_constraint(item):
                     pass_constraints = False
                     break
@@ -80,35 +90,61 @@ class DataSet:
 
             number_of_valid_data += 1
 
-            for constraint in self.constraints:
+            for constraint in self.constraints:  # constraints
                 if not constraint(item):
                     pass_constraints = False
                     break
+
             if pass_constraints:
                 filtered_data.append(item)
+
             if self.percentage_within_group:  # for percentage within group
                 key = self.x_function(item)
-                if key in all_appearance:
-                    all_appearance[key] += 1
-                else:
-                    all_appearance[key] = 1
+                all_appearance[key] = all_appearance.get(key, 0) + 1
 
+        # handle number_of_appearance, percentage and percentage_within_group
         if self.number_of_appearance or self.percentage or self.percentage_within_group:
             appearance = {}
             for item in filtered_data:
                 key = self.x_function(item)
-                if key in appearance:
-                    appearance[key] += 1
-                else:
-                    appearance[key] = 1
-            if self.percentage:
+                appearance[key] = appearance.get(key, 0) + 1
+
+            if self.percentage:  # handle percentage (divide each result by the number of data)
                 for key in appearance:
                     appearance[key] /= number_of_valid_data
-            if self.percentage_within_group:
+
+            if self.percentage_within_group:  # handle percentage within group
                 for key in appearance:
                     appearance[key] /= all_appearance[key]
-            return appearance
-        assert False
+
+            if self.single:
+                if len(appearance) != 1:
+                    raise ValueError("Single mode set while there are more than one result. "
+                                     "Results: " + str(appearance))
+                return next(iter(appearance.values()))
+            else:
+                return appearance
+
+        # handle y_function
+        if self.y_function:
+            values = {}
+            for item in filtered_data:
+                key = self.x_function(item)
+                if key in values:
+                    values[key].append(item)
+                else:
+                    values[key] = [item]
+
+            for key, value in values.items():
+                values[key] = self.y_function(value)
+
+            if self.single:
+                return next(iter(values.values()))
+            else:
+                return values
+
+        # neither any options nor y_function is set
+        raise ValueError("set_y not called when calling get_result")
 
     def get_series(self):
         return []
